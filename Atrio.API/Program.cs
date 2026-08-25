c81bb9ad529bd00f37ba6b1a0d95485643eab577
adeena-ff <adeena.anwarr@gmail.com>
2026-08-25 22:12:55 +0500

    fix: use available local API port

--- Atrio.API/Program.cs
+++ Atrio.API/Program.cs
@@ -47,7 +47,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(AllowFrontend, policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
